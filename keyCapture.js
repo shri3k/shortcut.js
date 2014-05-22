08910b9df176af288f66487f5ef811eae8960183
Yojan Shrestha <sickgrin@gmail.com>
2014-05-21 20:52:03 -0500

    Init commit

---
+++ keyCapture.js
@@ -0,0 +1,15 @@
+//keyCapture.js
+var obj = {
+    "super": ['x'],
+    "p": function(){
+        console.log("you pressed p");
+    }
+};
+
+function addShortcut(ob){
+    var key = Object.keys(ob);
+    document.onkeyup = function(e){
+        String.
+    };
+}
+addShortcut(obj);
