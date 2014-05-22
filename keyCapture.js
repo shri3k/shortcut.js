8653cda16b1c95924ca01b620e027307d4a297df
Yojan Shrestha <sickgrin@gmail.com>
2014-05-21 23:14:01 -0500

    The basic functionality is working

--- keyCapture.js
+++ keyCapture.js
@@ -1,15 +1,27 @@
 //keyCapture.js
 var obj = {
-    "super": ['x'],
-    "p": function(){
-        console.log("you pressed p");
-    }
+	'super': [
+		'x'
+	],
+	'p': function() {
+		console.log('you pressed p');
+	}
 };
 
-function addShortcut(ob){
-    var key = Object.keys(ob);
-    document.onkeyup = function(e){
-        String.
-    };
+function addShortcut(ob) {
+	state = false;
+	var key = Object.keys(ob);
+	document.onkeyup = function(e) {
+		if (state && e.key in obj) {
+			console.log(e.keyCode);
+		}
+		state = false;
+	};
+	document.onkeydown = function(e) {
+		if (!state && ob['super'].indexOf(e.key) > -1) {
+			state = true;
+			console.log(e.keyCode + 'supaaa!!');
+		}
+	};
 }
 addShortcut(obj);
